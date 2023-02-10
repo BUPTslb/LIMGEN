e4701f5989ec225154fda7dfae1fdbe7355ca3d4
shenlibo <shenlibo@bupt.cn>
2023-02-10 11:13:21 +0800

    cdfg develop

--- TriLogic/Func_main.cpp
+++ TriLogic/Func_main.cpp
@@ -41,6 +41,17 @@ int main()
     //使用剪枝算法进行DSE
     //没有使用过的逻辑/算术运算要进行拆解
     ********************************/
+
+    //**********各类型节点数量****************
+    int num_op=0;
+    int num_assign=0;
+    int num_loop=0;
+    int num_branch=0;
+    //构建键值对，存储
+    map<string,int> operator_num;//各个算子出现的次数
+    map<string,int> dst_id;//Dst出现的最后位置
+    map<string,int> type_num;
+    map<int,int> op_map;//存储op类型的id索引关系，input做键
     
     //1.先读一个节点让其输出
     ifstream file("../hdlAst/hdlast.json");
@@ -52,17 +63,14 @@ int main()
     d.Parse(content.c_str());
     // 测试rapidjson功能区*******************************************
         cout<<d[0]["Input"][1].GetString()<<endl;
-        cout<<"IsArray的功能："<<d[0]["Input"].IsArray()<<endl;
+        cout<<"IsArray的功能："<<d[1]["Input"].IsArray()<<endl;
         cout<<"IsString的功能："<<d[0]["Input"].IsString()<<endl;
         cout<<"IsString的功能："<<d[0]["Input"][1].IsString()<<endl;
         cout<<"IsString的功能："<<d[0]["Node_ID"].IsString()<<endl;
         cout<<"HasMember的功能："<<d[0].HasMember("Dst")<<endl;
     //***********************************************************    
     int json_size=d.Size();//整个数组的尺寸（节点的数量）
-    //构建键值对，存储
-    map<string,int> operator_num;//各个算子出现的次数
-    map<string,int> dst_id;//Dst出现的最后位置
-    map<string,int> type_num;
+    
     //遍历json文件
     cout<<"The number of CDFG = "<<json_size<<endl;
     cout<<d[0]["Type"].GetString()<<endl;
@@ -73,12 +81,6 @@ int main()
     //进行重新排序，然后再进行逻辑模式的套用  
     ************************************************/
 
-    //**********各类型节点数量****************
-    int num_op=0;
-    int num_assign=0;
-    int num_loop=0;
-    int num_branch=0;
-    //**************************************
     for(int i=0;i<json_size;i++){
 
         if(d[i]["Type"].GetString()== op)
@@ -100,11 +102,11 @@ int main()
    {
        cout<<it->first<<" "<<it->second<<endl;
    }
-   cout<<"各个Dst操作数的位置"<<endl;
-    for(map<string,int>::iterator it=dst_id.begin();it!=dst_id.end();it++)
-   {
-       cout<<it->first<<" "<<it->second<<endl;
-   }
+//    cout<<"各个Dst操作数的位置"<<endl;
+//     for(map<string,int>::iterator it=dst_id.begin();it!=dst_id.end();it++)
+//    {
+//        cout<<it->first<<" "<<it->second<<endl;
+//    }
    /*****************************
    //关键算法
    //算子调度
@@ -124,62 +126,65 @@ int main()
         //2.判断input是否为之前节点的Dst
         //使用的数据结构：？
         // cout<<"Typeof Input:"<<typeid(d[1]["Input"]).name()<<endl;
-        for (size_t j = i-1; j >= 0; j--)
+        int num_input;
+        int type_id;
+
+            //Input的数量和类型
+            //Branch和loop没有input
+            //如果有“Input”，得到input的数量
+        type_id=Type2node(d[i]["Type"].GetString());
+        map<int,bool> input_depend;//构建hash表，存储当前输入，如果前面找到了前驱依赖，值为true
+        if(d[i].HasMember("Input"))
         {
+            num_input=d[i]["Input"].IsArray() ? d[i]["Input"].Size() : 1; 
+        }
+        
+
+        for (int j = i-1; j >= 0; j--)//j比i小
+        {
+            //如果j有输出，j的Dst位置更新到hash表中
             if(d[j].HasMember("Dst"))
             {
                 string key_dst=d[j]["Dst"].GetString();
-                if(dst_id[key_dst]&&(dst_id[key_dst]<=i))//已经存在，且值比i+1小
+                if(dst_id[key_dst]&&(dst_id[key_dst]>j+1))//已经存在，且值比j大
                     break;
                 else //不存在，或者位置错了
                     dst_id[key_dst]=j+1;
             }
-            //Input的数量和类型
-            //Branch和loop没有input
-            //如果有“Input”，得到input的数量
-            int num_input;
-            if(d[j].HasMember("Input"))
-                num_input=d[j]["Input"].IsArray() ? d[j]["Input"].Size() : 1;
             
-            switch (Type2node(d[j]["Type"].GetString()))
+            switch (type_id)
             {
-            case 1://类型为Op
+            case 1://类型为Op，input的类型都是string
             {
                 //设定：输入最多为两个
-                string name_dst;
+                // string name_dst;
                 if(num_input==2)
                 {    
-                    string name_in1=d[j]["Input"][0].GetString();
-                    string name_in2=d[j]["Input"][1].GetString();
+                    string name_in1=d[i]["Input"][0].GetString();
+                    string name_in2=d[i]["Input"][1].GetString();
                     int id_dst;
-                    if(dst_id[name_in1])//存在
-                        cout<<"存在数据依赖："<<j+1<<"->"<<dst_id[name_in1]<<endl;
-                    if(dst_id[name_in2])//存在
-                        cout<<"存在数据依赖："<<j+1<<"->"<<dst_id[name_in2]<<endl;
-          
-                    // if(d[i].HasMember("Dst") ? d[i]["Dst"].IsString():0)
-                    // {
-                    //         name_dst=d[i]["Dst"].GetString();//Dst是string类型，有的没有dst
-                    //         if(name_in1==name_dst||name_in2==name_dst)
-                    //         cout<<"存在数据依赖："<<j+1<<"->"<<i+1<<endl;
-                    // }                   
+                    if(dst_id[name_in1]&&(!input_depend[0]))//存在且未被输出
+                    {   
+                        input_depend[0]=true;
+                        cout<<"输入数量为string2，in1存在数据依赖："<<dst_id[name_in1]<<"->"<<i+1<<"  j= "<<j<<endl;
+                    }
+                    if(dst_id[name_in2]&&(!input_depend[1]))//存在
+                    {
+                        input_depend[1]=true;
+                        cout<<"输入数量为string2，in2存在数据依赖："<<dst_id[name_in2]<<"->"<<i+1<<"  j="<<j<<endl;
+                        break;  
+                    }      
                 }
-                else //输入数量为1，可能是string也可能是int
+                else //Op输入数量为1
                 {
-                    //判断
-                    if(d[j]["Input"][0].IsInt())
+                    
+                    string input_name=d[i]["Input"][0].GetString();
+                    if(dst_id[input_name])//存在
                     {
-                        int input_id= d[j]["Input"][0].GetInt();
-                        if(input_id==i+1)
-                            cout<<"存在数据依赖："<<j+1<<"->"<<i+1<<endl;
+                        input_depend[0]=true;
+                        cout<<"输入数量为string1，存在数据依赖："<<j+1<<"->"<<dst_id[input_name]<<endl;
                     }
-                    else
-                    {
-                        string input_name=d[j]["Input"][0].GetString();
-                        if(dst_id[input_name])//存在
-                            cout<<"存在数据依赖："<<j+1<<"->"<<dst_id[input_name]<<endl;
- 
-                    }
+                    
                 }
             }
                 break;
